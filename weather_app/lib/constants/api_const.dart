c0b7a96fb17be78ff691c9f8412c021e5cf97786
Zhanyl Kubatbek kyzy <kubatbekova.janyl98@gmail.com>
2023-11-13 15:20:32 +0100

    Weather app and API

---
+++ weather_app/lib/constants/api_const.dart
@@ -0,0 +1,5 @@
+class ApiConst {
+  static const apiKey = ' b7623297cc044ff3afd125748231311';
+  static const example =
+      'https://api.weatherapi.com/v1/current.json?key=b7623297cc044ff3afd125748231311&q=Stockholm';
+}
--- weather_app/lib/home/home_page.dart
+++ weather_app/lib/home/home_page.dart
@@ -1,8 +1,15 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:http/http.dart';
 import 'package:weather_app/constants/assets_const.dart';
 import 'package:weather_app/constants/colors_const.dart';
 import 'package:weather_app/home/home_body.dart';
+import 'package:weather_app/model/weather_model.dart';
+
+const url =
+    'https://api.weatherapi.com/v1/current.json?key=b7623297cc044ff3afd125748231311&q=Stockholm';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,6 +19,19 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Future<Weather?> getWeatherData() async {
+    try {
+      final uri = Uri.parse(url);
+      final client = Client();
+      final response = await client.get(uri);
+      final data = jsonDecode(response.body) as Map<String, dynamic>;
+      final weather = Weather.fromJson(data);
+      return weather;
+    } catch (e) {
+      throw Exception('Bit kata boldu, kata: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,9 +64,28 @@ class _HomePageState extends State<HomePage> {
             ],
           ),
         ),
-        child: const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-          child: HomeBody(),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+          child: FutureBuilder<Weather?>(
+            future: getWeatherData(),
+            builder: (context, snapshot) {
+              // `context` widget daragybyzdyn kaisyl jerinde bolgonubuzdu beret
+              // `snapshot` bizdin datanym abaly M:
+              // - kutup jatabyz,
+              // - iygiliktuu data keldi,
+              // - Error bolup data kelgen jok
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator.adaptive();
+              } else if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.hasData) {
+                  return HomeBody(weather: snapshot.data!);
+                } else if (snapshot.hasError) {
+                  return Text('Bir kata boldu kata: ${snapshot.error}');
+                }
+              }
+              return const SizedBox.shrink();
+            },
+          ),
         ),
       ),
     );
