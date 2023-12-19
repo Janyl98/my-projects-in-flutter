29d7fddb399cdceed149ed2de016687c2d22796d
Zhanyl Kubatbek kyzy <kubatbekova.janyl98@gmail.com>
2023-12-19 02:39:48 +0100

    added chat app

--- mychatapp/lib/app/myapp.dart
+++ mychatapp/lib/app/myapp.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:mychatapp/home/home_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mychatapp/app/bloc/auth_bloc.dart';
+import 'package:mychatapp/wellcome/wellcome.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return BlocProvider(
+      create: (context) => AuthBloc(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const WellComeView(),
       ),
-      home: const HomePage(),
     );
   }
 }
--- mychatapp/lib/home/home_page.dart
+++ mychatapp/lib/home/home_page.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mychatapp/app/bloc/auth_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: BlocBuilder<AuthBloc, AuthState>(
+          builder: (context, state) {
+            if (state is AuthenticatedState) {
+              return Text(state.user.email);
+            } else {
+              return const Text('Home');
+            }
+          },
+        ),
+      ),
+    );
   }
 }
